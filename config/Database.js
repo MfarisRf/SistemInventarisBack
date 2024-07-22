ebe671ca5c7f86436865147c478e0de3614cb2cb
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2024-07-22 20:53:55 +0700

    db conf

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_inventory"
+    host: "sql110.infinityfree.com",
+    user: "if0_36953085",
+    password: "iQ1VB6pAYUmqi",
+    database: "if0_36953085_db_inventory"
 })
 
 export default db;
