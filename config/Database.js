fa5114a6d5783ecfe73d59da3ccc66e6af9c9251
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2024-07-22 21:51:48 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
-        return;
+        process.exit(1); // Keluar dari aplikasi jika tidak dapat terhubung
     }
     console.log('Connected to the database.');
 });
